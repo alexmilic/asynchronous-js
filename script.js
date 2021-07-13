2aa3edb1f15c06ee8854c28447416dd6f575221c
Aleksandar Milic <alexmilic01@gmail.com>
2021-07-13 22:44:35 +0200

    Use async,await

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 'use strict';
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
-// const renderCountry = function(data, className = '') {
-//     const html = `
-//         <article class="country ${className}">
-//         <img class="country__img" src="${data.flag}" />
-//         <div class="country__data">
-//             <h3 class="country__name">${data.name}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${Number((data.population / 1000000).toFixed(1))} people</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class="country__row"><span>💰</span>C${data.currencies[0].name}UR</p>
-//         </div>
-//         </article>
-//     `;
+const renderCountry = function(data, className = '') {
+    const html = `
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${Number((data.population / 1000000).toFixed(1))} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>C${data.currencies[0].name}UR</p>
+        </div>
+        </article>
+    `;
 
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-// }
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+}
 
 // const renderError = function(msg) {
 //     countriesContainer.insertAdjacentText('beforeend', msg);
@@ -83,40 +83,61 @@
 //     return
 // });
 
-const lotteryPromise = new Promise(function(resolve, reject) {
-    console.log(`Lattery draw is happening!`);
-    setTimeout(() => {
-        if(Math.random() >= 0.5) {
-            resolve(`You win 💰`)
-        } else {
-            reject(new Error(`You lost your money 🗣️`));
-        }
-    }, 2000);
-});
+// const lotteryPromise = new Promise(function(resolve, reject) {
+//     console.log(`Lattery draw is happening!`);
+//     setTimeout(() => {
+//         if(Math.random() >= 0.5) {
+//             resolve(`You win 💰`)
+//         } else {
+//             reject(new Error(`You lost your money 🗣️`));
+//         }
+//     }, 2000);
+// });
 
-lotteryPromise
-.then(response => console.log(response))
-.catch(error => console.error(error))
+// lotteryPromise
+// .then(response => console.log(response))
+// .catch(error => console.error(error))
 
-const wait = function(seconds) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, seconds * 1000);
-    });
+// const wait = function(seconds) {
+//     return new Promise(function(resolve) {
+//         setTimeout(resolve, seconds * 1000);
+//     });
+// }
+
+// wait(1).then(() => {
+//     console.log(`I waited for 1 seconds`);
+//     return wait(1);
+// })
+// .then(() => {
+//     console.log(`I waited for 2 seconds`);
+//     return wait(1);
+// })
+// .then(() => {
+//     console.log(`I waited for 3 seconds`);
+//     return wait(1);
+// })
+// .then(() => {
+//     console.log(`I waited for 4 seconds`);
+//     return wait(1);
+// });
+
+const getPosition = function() {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
 }
 
-wait(1).then(() => {
-    console.log(`I waited for 1 seconds`);
-    return wait(1);
-})
-.then(() => {
-    console.log(`I waited for 2 seconds`);
-    return wait(1);
-})
-.then(() => {
-    console.log(`I waited for 3 seconds`);
-    return wait(1);
-})
-.then(() => {
-    console.log(`I waited for 4 seconds`);
-    return wait(1);
-});
+const whereAmI = async function() {
+    const pos = await getPosition();
+    const {latitude: lat, longitude: lng} = pos.coords;
+
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    const dataGeo = await resGeo.json();
+    
+    const res = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+    const data = await res.json();
+    renderCountry(data[0]);
+    countriesContainer.style.opacity = 1;
+}
+
+whereAmI();
